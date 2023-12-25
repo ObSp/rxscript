fbfb1f876ae50f04e9f4cbcebca9eeeec9729e61
ObSp <150492054+ObSp@users.noreply.github.com>
2023-12-25 18:37:47 -0500

    Create InstanceTypes.lua

---
+++ libraries/InstanceTypes.lua
@@ -0,0 +1,16 @@
+-- Not much yet, more will be added as I continue writing modules for rxscript, as of now you can just make your own InstanceTypes like this!
+
+local module = {}
+
+function module.DataStoreSettings(AutoConvertToString: boolean, AutoKickPlayers: boolean, ErrorKickMessage: string, AutoCachePlayers,CacheCycleTime: number)
+	return {
+		AutoConvertToString = AutoConvertToString or false, 
+		AutoKickPlayers = AutoConvertToString or false,
+		ErrorKickMessage=ErrorKickMessage or "Your data has failed to load, please rejoin the game",
+		AutoCachePlayers = AutoCachePlayers or false,
+		CacheCycleTime = CacheCycleTime or 60
+	}
+end
+
+
+return module
