ca76eaa9ba5d0640d2f44245356b03d07a32144b
ObSp <150492054+ObSp@users.noreply.github.com>
2023-12-25 18:19:33 -0500

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,44 @@
+-- SHIFTLOCK IS UNFINISHED
+
+local module = {}
+
+module.WaitFor = require(script.WaitFor)
+
+module.Switch = require(script.Switch)
+
+module.Timer = require(script.Timer)
+
+module.Wait = require(script.Wait)
+
+module.SortTableByValue = require(script.SortTableByValue)
+
+module.Find = require(script.Find)
+
+module.CreateLeaderstats = require(script.CreateLeaderstats)
+
+module.CreateZone = require(script.CreateZone)
+
+module.GetDataStore = require(script.GetDataStore)
+
+module.NewInstance = require(script.NewInstance)
+
+module.CustomInstance = require(script.CustomInstance)
+
+module.Input = require(script.Input)
+
+module.RunWithDelay = require(script.RunWithDelay)
+
+module.RemoveFromTable = require(script.RemoveFromTable)
+
+module.Camera = require(script.Camera)
+
+module.ShiftLock = require(script.ShiftLock)
+
+module.Repeat = require(script.Repeat)
+
+module.Check = require(script.Check)
+
+
+module._InstanceTypes = require(script.InstanceTypes)
+
+return module
