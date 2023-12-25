1d2962dec77b97dd1a0dadbb851f45275e0f9fcb
ObSp <150492054+ObSp@users.noreply.github.com>
2023-12-25 18:32:17 -0500

    Create CustomInstance.lua

---
+++ libraries/CustomInstance.lua
@@ -0,0 +1,33 @@
+local InstanceTypes = require(script.Parent.InstanceTypes)
+local customInstanceMethods = {}
+customInstanceMethods.__index = customInstanceMethods
+customInstanceMethods.__tostring = function(self)
+	return self.Name or `Attempted to print nonexistent instance`
+end
+
+
+function customInstanceMethods:Destroy()
+	if self._isrbx then self.Instance:Destroy() end
+	for prop,_ in self do
+		self[prop] = nil
+	end
+	return self
+end
+
+return function(Name, rbxInstance, Properties: {})
+	local newCustom = setmetatable({}, customInstanceMethods)
+	newCustom.Name = Name
+	
+	newCustom.Instance = rbxInstance
+	if typeof(rbxInstance) == "Instance" then
+		newCustom._isrbx = true
+	else
+		newCustom._isrbx = false
+	end
+	
+	for property,value in Properties do
+		newCustom[property] = value
+	end
+	
+	return newCustom
+end
